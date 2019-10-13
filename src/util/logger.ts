42f2a69fcbeac04365a108feca66043a6bbf1194
ceofred <johnsonmessilo19@gmail.com>
2019-10-13 15:40:22 +0100

    fix logger

--- src/util/logger.ts
+++ src/util/logger.ts
@@ -1,18 +1,44 @@
-import { Logger, LoggerOptions, transports } from "winston";
+import winston from "winston";
 
-const options: LoggerOptions = {
+// const options: LoggerOptions = {
+//     transports: [
+//         new transports.Console({
+//             level: process.env.NODE_ENV === "production" ? "error" : "debug"
+//         }),
+//         new transports.File({ filename: "debug.log", level: "debug" })
+//     ]
+// };
+
+// const logger = new Logger(options);
+
+// if (process.env.NODE_ENV !== "production") {
+//     logger.debug("Logging initialized at debug level");
+// }
+
+// export default logger;
+
+
+const logger = winston.createLogger({
+    level: "info",
+    format: winston.format.json(),
+    defaultMeta: { service: "user-service" },
     transports: [
-        new transports.Console({
-            level: process.env.NODE_ENV === "production" ? "error" : "debug"
-        }),
-        new transports.File({ filename: "debug.log", level: "debug" })
+        //
+        // - Write to all logs with level `info` and below to `combined.log` 
+        // - Write all logs error (and below) to `error.log`.
+        //
+        new winston.transports.File({ filename: "debug.log", level: "debug" }),
     ]
-};
+});
 
-const logger = new Logger(options);
-
+//
+// If we're not in production then log to the `console` with the format:
+// `${info.level}: ${info.message} JSON.stringify({ ...rest }) `
+// 
 if (process.env.NODE_ENV !== "production") {
-    logger.debug("Logging initialized at debug level");
+    logger.add(new winston.transports.Console({
+        format: winston.format.simple()
+    }));
 }
 
 export default logger;
